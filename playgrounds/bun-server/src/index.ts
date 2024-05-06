affd01ee561d2c3a7e877522b16f3530fe568228
croatialu <wuli.croatia@foxmail.com>
2024-05-06 16:07:01 +0800

    update

--- playgrounds/bun-server/src/index.ts
+++ playgrounds/bun-server/src/index.ts
@@ -1,4 +1,7 @@
 import type { ServerWebSocket } from "bun"
+import wrtc from '@roamhq/wrtc'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc';
 
 const wsReadyStateConnecting = 0
 const wsReadyStateOpen = 1
@@ -6,14 +9,15 @@ const wsReadyStateOpen = 1
 
 interface WSSubscribePayload {
   type: 'subscribe'
+  topics: string[]
 }
 interface WSUnsubscribePayload {
   type: 'unsubscribe'
+  topics: string[]
 }
 interface WSPublishPayload {
   type: 'publish'
   topic: string
-  data: any
 }
 
 interface WSPingPayload {
@@ -45,10 +49,23 @@ const setConnRecord = (ws: WebSocketConn, record: Partial<ConnRecord>) => {
     ...record
   })
 }
-const removeConnRecord = (ws: WebSocketConn) => {
+
+function removeConnRecord(ws: WebSocketConn) {
+  const subscribedTopics = getConnRecord(ws).subscribedTopics
+
+  subscribedTopics.forEach(topicName => {
+    const subs = topics.get(topicName) || new Set()
+    subs.delete(ws)
+    if (subs.size <= 1) {
+      topics.delete(topicName)
+      const ydoc = ydocs.get(topicName)
+      ydoc?.destroy()
+      ydocs.delete(topicName)
+    }
+  })
+  subscribedTopics.clear()
   connRecords.delete(ws)
 }
-
 
 
 const topics = new Map<string, Set<WebSocketConn>>()
@@ -83,6 +100,39 @@ const send = (conn: WebSocketConn, message: any) => {
   }
 }
 
+const ydocs = new Map<string, Y.Doc>()
+const initWebrtcProvider = (topic: string) => {
+  if (ydocs.get(topic))
+    return ydocs.get(topic)
+
+  console.log('init webrtc provider', topic)
+  const ydoc = new Y.Doc()
+  ydocs.set(topic, ydoc)
+  const provider = new WebrtcProvider(topic, ydoc, {
+    signaling: ['ws://localhost:3000'],
+
+    peerOpts: {
+      wrtc: {
+        RTCSessionDescription: wrtc.RTCSessionDescription,
+        RTCPeerConnection: wrtc.RTCPeerConnection,
+        RTCIceCandidate: wrtc.RTCIceCandidate,
+      },
+    }
+  })
+
+  ydoc.on('update', () => {
+    console.log('update', ydoc.getArray('state').toJSON())
+  })
+
+  ydoc.on('destroy', () => {
+    provider.destroy()
+  })
+
+
+  return ydoc
+}
+
+
 
 
 Bun.serve<WebSocketPayload>({
@@ -96,17 +146,44 @@ Bun.serve<WebSocketPayload>({
   },
   websocket: {
     message(ws, message) {
-      const messageData = ws.data
-
+      const messageData = JSON.parse(message  as any) as WebSocketPayload
+      console.log(messageData, 'messageData')
       switch (messageData.type) {
         case 'ping':
           send(ws, { type: 'pong' })
           break
-        case 'publish':
+        case 'publish': {
+          const { topic } = messageData
+          const subscribers = topics.get(topic)
+          if (subscribers) {
+            const size = subscribers.size
+            subscribers.forEach((subscriber) => {
+              send(subscriber, {
+                ...messageData,
+                clients: size,
+              })
+            })
+          }
+        }
           break
         case 'subscribe':
+          (messageData.topics || []).forEach((topicName) => {
+            const { subscribedTopics } = getConnRecord(ws)
+            subscribedTopics.add(topicName)
+
+            const topic = topics.get(topicName) || new Set()
+            topic.add(ws)
+            topics.set(topicName, topic)
+
+            initWebrtcProvider(topicName)
+          })
           break
         case 'unsubscribe':
+          (messageData.topics || []).forEach((topicName) => {
+            const subs = topics.get(topicName)
+
+            subs?.delete(ws)
+          })
           break
       }
 
@@ -123,18 +200,11 @@ Bun.serve<WebSocketPayload>({
       setConnRecord(ws, { pongReceived: true })
     },
     close(ws, code, message) {
-      const { subscribedTopics } = getConnRecord(ws)
-      subscribedTopics.forEach(topicName => {
-        const subs = topics.get(topicName) || new Set()
-        subs.delete(ws)
-        if (subs.size === 0) {
-          topics.delete(topicName)
-        }
-      })
-
-      subscribedTopics.clear()
+       
       removeConnRecord(ws)
     }, // a socket is closed
     drain(ws) { }, // the socket is ready to receive more data
   }
 })
+
+console.log('server started on port 3000')
---
+++ playgrounds/node-server/src/index.ts
@@ -0,0 +1,227 @@
+import http from 'node:http'
+import { WebSocketServer, WebSocket } from 'ws';
+import wrtc from '@roamhq/wrtc'
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc';
+
+Object.assign(global, { WebSocket })
+
+const wsReadyStateConnecting = 0
+const wsReadyStateOpen = 1
+
+interface WSSubscribePayload {
+  type: 'subscribe'
+  topics: string[]
+}
+interface WSUnsubscribePayload {
+  type: 'unsubscribe'
+  topics: string[]
+}
+interface WSPublishPayload {
+  type: 'publish'
+  topic: string
+}
+
+interface WSPingPayload {
+  type: 'ping'
+}
+
+type WebSocketPayload =
+  | WSSubscribePayload
+  | WSUnsubscribePayload
+  | WSPublishPayload
+  | WSPingPayload
+
+type WebSocketConn = WebSocket
+
+interface ConnRecord {
+  subscribedTopics: Set<string>
+  pongReceived: boolean
+}
+
+const connRecords = new Map<WebSocketConn, ConnRecord>()
+
+function getConnRecord(ws: WebSocketConn) {
+  if (!connRecords.get(ws))
+    connRecords.set(ws, { pongReceived: false, subscribedTopics: new Set() })
+
+
+  return connRecords.get(ws)!
+}
+function setConnRecord(ws: WebSocketConn, record: Partial<ConnRecord>) {
+  connRecords.set(ws, {
+    ...connRecords.get(ws) as any,
+    ...record,
+  })
+}
+function removeConnRecord(ws: WebSocketConn) {
+  const subscribedTopics = getConnRecord(ws).subscribedTopics
+
+  subscribedTopics.forEach(topicName => {
+    const subs = topics.get(topicName) || new Set()
+    subs.delete(ws)
+    console.log(subs.size, 'subs.size')
+    if (subs.size <= 1) {
+      topics.delete(topicName)
+      const ydoc = ydocs.get(topicName)
+      ydoc?.destroy()
+      ydocs.delete(topicName)
+    }
+  })
+  subscribedTopics.clear()
+  connRecords.delete(ws)
+}
+
+const topics = new Map<string, Set<WebSocketConn>>()
+const ydocs = new Map<string, Y.Doc>()
+
+function startPingPong(ws: WebSocketConn) {
+  const pingInterval = setInterval(() => {
+    const { pongReceived } = getConnRecord(ws)
+    if (!pongReceived) {
+      ws.close()
+      clearInterval(pingInterval)
+    }
+    else {
+      setConnRecord(ws, { pongReceived: false })
+      try {
+        ws.ping()
+      }
+      catch (e) {
+        ws.close()
+      }
+    }
+  }, 1000 * 60)
+}
+
+function send(conn: WebSocketConn, message: any) {
+  if (conn.readyState !== wsReadyStateConnecting && conn.readyState !== wsReadyStateOpen)
+    conn.close()
+
+  try {
+    conn.send(JSON.stringify(message))
+  }
+  catch (e) {
+    conn.close()
+  }
+}
+
+const initWebrtcProvider = (topic: string) => {
+  if (ydocs.get(topic))
+    return ydocs.get(topic)
+
+  console.log('init webrtc provider', topic)
+  const ydoc = new Y.Doc()
+  ydocs.set(topic, ydoc)
+  const provider = new WebrtcProvider(topic, ydoc, {
+    signaling: ['ws://localhost:3000'],
+
+    peerOpts: {
+      wrtc: {
+        RTCSessionDescription: wrtc.RTCSessionDescription,
+        RTCPeerConnection: wrtc.RTCPeerConnection,
+        RTCIceCandidate: wrtc.RTCIceCandidate,
+      },
+    }
+  })
+
+  ydoc.on('update', () => {
+    console.log('update', ydoc.getArray('state').toJSON())
+  })
+
+  ydoc.on('destroy', () => {
+    provider.destroy()
+  })
+
+
+  return ydoc
+}
+
+
+const wss = new WebSocketServer({
+  port: 3000,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+
+  }
+});
+
+
+wss.on('connection', (ws) => {
+  setConnRecord(ws, {
+    pongReceived: true,
+    subscribedTopics: new Set()
+  })
+
+  startPingPong(ws)
+
+
+  ws.on('message', (data) => {
+    const message = JSON.parse(data.toString()) as WebSocketPayload
+    switch (message.type) {
+      case 'subscribe':
+        (message.topics || []).forEach((topicName) => {
+          if (typeof topicName === 'string') {
+            initWebrtcProvider(topicName)
+
+            const subscribedTopics = getConnRecord(ws).subscribedTopics
+            
+            const topic = topics.get(topicName) || new Set()
+            topic.add(ws)
+            topics.set(topicName, topic)
+
+            subscribedTopics.add(topicName)
+          }
+        })
+        break
+      case 'unsubscribe':
+        (message.topics || []).forEach((topicName) => {
+          const subs = topics.get(topicName)
+
+          subs?.delete(ws)
+        })
+        break
+      case 'publish': {
+        const { topic } = message
+        const subscribers = topics.get(topic)
+        if (subscribers) {
+          const size = subscribers.size
+          console.log('publish', topic, size)
+          subscribers.forEach((subscriber) => {
+            send(subscriber, {
+              ...message,
+              clients: size,
+            })
+          })
+        }
+      }
+        break
+      case 'ping':
+        setConnRecord(ws, { pongReceived: true })
+        break
+    }
+  })
+
+  ws.on('close', () => {
+    removeConnRecord(ws)
+
+  })
+})
+
+console.log('Server started at ws://localhost:3000')
